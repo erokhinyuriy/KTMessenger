ac9b3850577723ec5e44479f82e4661e5ac77e3f
YuriyErokhin <erokhin2013@mail.ru>
2021-02-27 23:38:42 +0500

    5. Refactoring

---
+++ app/src/main/java/com/example/ktmessenger/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.ktmessenger.ui.fragments
+
+import com.example.ktmessenger.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/com/example/ktmessenger/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.ktmessenger.ui.fragments
+
+import com.example.ktmessenger.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+}
