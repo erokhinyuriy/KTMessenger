218d7c11a5bd44d54a925ad41922505442ac2783
YuriyErokhin <erokhin2013@mail.ru>
2021-03-03 00:04:26 +0500

    7. Added menu to settings (w.i.p)

--- app/src/main/java/com/example/ktmessenger/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/ktmessenger/ui/fragments/SettingsFragment.kt
@@ -1,11 +1,17 @@
 package com.example.ktmessenger.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.ktmessenger.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
